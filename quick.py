377f959986c1cef6390bf721baf2a255f0adaf66
choijaewu <jawoo053@gmail.com>
2024-09-08 20:51:44 +0900

    test

---
+++ quick.py
@@ -0,0 +1,29 @@
+def tostring(li): #리스트를 문자열로 변환(글자 사이 공백 추가)
+    answer = ''
+    for i in li:
+        answer = answer + str(i) + ' '
+    return answer.rstrip()
+
+def quicksort(li):
+    if len(li) <= 1:
+        return li
+
+    left = []
+    right = []
+    pivot = li[-1]
+    for element in li[:-1]:
+        if element < pivot:
+            left.append(element)
+        else:
+            right.append(element)
+    
+    strleft = tostring(left)
+    strright = tostring(right)
+    print('\033[93m' + strleft, '\033[96m' + "/" + str(pivot) + "/" , '\033[91m' + strright)
+    result = quicksort(left) + [pivot] + quicksort(right)
+    return result
+    
+Input = [3, 8, 5, 6, 9, 1 ,4, 2, 7]
+Output = quicksort(Input)
+stringOutput = tostring(Output)
+print('\033[37m' + stringOutput)
